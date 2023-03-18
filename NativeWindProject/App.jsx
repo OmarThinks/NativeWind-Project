be0811b2a9bb7c4e6c40165d6cb0689fcedfb468
Omar Magdy <24939461+OmarThinks@users.noreply.github.com>
2023-03-18 15:21:05 +0200

    tested config in subfolder

--- NativeWindProject/App.jsx
+++ NativeWindProject/App.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 
 const App = () => {
   return (
-    <View>
-      <Text>App</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text>Open up App.js to start working on your app!</Text>
     </View>
   );
 };
--- NativeWindProject/tailwind.config.js
+++ NativeWindProject/tailwind.config.js
@@ -1,6 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./App.{js,jsx,ts,tsx}', './<custom-folder>/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {},
   },
