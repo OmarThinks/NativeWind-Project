5c4f67af3d1749c6bb785c5aa7f846cb0c000ffa
OmarThinks <24939461+OmarThinks@users.noreply.github.com>
2023-08-05 23:36:46 +0300

    More Styling

--- NativeWindProject/src/App.tsx
+++ NativeWindProject/src/App.tsx
@@ -3,8 +3,10 @@ import React from 'react';
 
 const App = () => {
   return (
-    <View className="bg-red-500 flex-1">
-      <Text>App</Text>
+    <View className="bg-black flex-1 justify-center align-middle">
+      <Text className="text-green-300 text-3xl font-bold underline bg-green-950 text-center shrink self-center">
+        App
+      </Text>
     </View>
   );
 };
